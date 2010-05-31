578f3a48ac0ae8cf5c4c1fb0c319d56252d9578a
laerciovitorino <laerciovitorino@5415766b-069b-cbca-9d6d-dede6f4b237b>
2010-05-31 04:10:01 +0000

    - Inseri os testes para a classe Paciente; - Alterei algumas partes da classe Paciente; - Dei continuidade a implementacao da classe Anamnese, iniciada por Antonio Ricardo.

--- trunk/JNutriClinica/src/JNutriClinica/Anamnese.java
+++ trunk/JNutriClinica/src/JNutriClinica/Anamnese.java
@@ -1,5 +1,25 @@
 package JNutriClinica;
 
-public class Anamnese {
+public class Anamnese{
+	
+	private float pesoUsual;
+	private float pesoIdeal;
+	private boolean perdeuPeso;
+	private String sintomasGastrointestinais;
+	private String patologiaChave;
+
+	public Anamnese(float pesoUsual, float pesoIdeal, boolean perdeuPeso, String sintomasGastrointestinais, String patologiaChave){
+		this.pesoUsual = pesoUsual;
+		this.pesoIdeal = pesoIdeal;
+		this.perdeuPeso = perdeuPeso;
+		this.sintomasGastrointestinais = sintomasGastrointestinais;
+		this.patologiaChave = patologiaChave;
+	}
+	
+	public enum patologiaChave{
+		DIABETES, CARDIOPATIA, PATOLOGIA_NO_FIGADO, PATOLOGIA_RENAL;
+	}
+	
+	//TODO: Implementar os demais metodos desta classe
 
 }
