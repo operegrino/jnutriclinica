9bd50a3ed5af86e9653b6cf9c57900c7134cb119
antonioricardojr <antonioricardojr@5415766b-069b-cbca-9d6d-dede6f4b237b>
2010-06-16 18:54:51 +0000

    add da classe Mulher

--- JNutriClinica/src/JNutriClinica/Mulher.java
+++ JNutriClinica/src/JNutriClinica/Mulher.java
@@ -2,40 +2,30 @@ package JNutriClinica;
 
 import java.util.Calendar;
 
-/**
- * Classe Homem - Um tipo de paciente.
- * 
- * @author Antonio Ricardo
- * 
- *@version 1.0
- */
+public class Mulher extends Pessoa implements Paciente {
 
-public class Homem extends Pessoa implements Paciente {
-
-	public Homem(String nome, int idade, String cpf, String endereco,
+	public Mulher(String nome, int idade, String cpf, String endereco,
 			Calendar dataDeNascimento, String telefone, Anamnese anamnese)
 			throws Exception {
 		super(nome, idade, cpf, endereco, dataDeNascimento, telefone, anamnese);
-
+		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public double getFatorAtividadeFisica() {
-
+		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
 	public double getIMC() {
-
 		return getAnamnese().getPesoUsual()
 				/ (anamnese.getAltura() * anamnese.getAltura());
 	}
 
 	@Override
 	public double getPesoTeorico() {
-
-		return anamnese.getAltura() * anamnese.getAltura() * 22.0;
+		return anamnese.getAltura() * anamnese.getAltura() * 20.8;
 	}
 
 	@Override
@@ -53,13 +43,13 @@ public class Homem extends Pessoa implements Paciente {
 	@Override
 	public double getTMB() {
 		if (18 <= idade && idade < 30) {
-			return 15.3 * anamnese.getPesoUsual() + 679;
+			return 14.7 * anamnese.getPesoUsual() + 496;
 		}
 		if (30 <= idade && idade < 60) {
-			return 11.6 * anamnese.getPesoUsual() + 879;
+			return 8.7 * anamnese.getPesoUsual() + 829;
 		}
 		if (idade >= 60) {
-			return 13.5 * anamnese.getPesoUsual() + 487;
+			return 10.5 * anamnese.getPesoUsual() + 596;
 		}
 		return 0;
 	}
