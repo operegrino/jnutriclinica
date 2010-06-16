9533c97f5a095c7394f37c649ffbab4a06bf460a
antonioricardojr <antonioricardojr@5415766b-069b-cbca-9d6d-dede6f4b237b>
2010-06-16 14:29:15 +0000

    add da classe consulta

--- JNutriClinica/src/JNutriClinica/Paciente.java
+++ JNutriClinica/src/JNutriClinica/Paciente.java
@@ -25,7 +25,7 @@ public class Paciente implements Pessoa {
 			Calendar dataDeNascimento, String telefone) {
 		setNome(nome);
 		setIdade(idade);
-		setCpf(cpf);
+		setCpf(formataCpf(cpf));
 		setEndereco(endereco);
 		setDataDeNascimento(dataDeNascimento);
 		setTelefone(telefone);
@@ -102,6 +102,11 @@ public class Paciente implements Pessoa {
 	public void setTelefone(String novoTelefone) {
 		this.telefone = novoTelefone;
 
+	}
+	
+	private String formataCpf(String cpf) {
+		return (cpf.substring(0, 3) + "." + cpf.substring(3, 6) + "."
+				+ cpf.substring(6, 9) + "-" + cpf.substring(9, 11));
 	}
 
 }
