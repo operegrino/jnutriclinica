cdd2f4b374ac65d1f5fac8067ce0112c3d53c4f0
antonioricardojr <antonioricardojr@5415766b-069b-cbca-9d6d-dede6f4b237b>
2010-07-06 02:01:27 +0000

    Introdução à classe Sistema que realizará as operações do Sistema.

---
+++ JNutriClinica/src/JNutriClinica/Sistema.java
@@ -0,0 +1,45 @@
+package JNutriClinica;
+
+import Anamnese;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Calendar;
+
+import data.DataBase;
+
+public class Sistema {
+	
+	public ArrayList<Pessoa> users = new ArrayList<Pessoa>();
+	DataBase bd = DataBase.getInstance();
+	
+	public void criaPaciente(String nome, int idade, String cpf, String endereco,
+			Calendar dataDeNascimento, String telefone, Anamnese anamnese, String genero) throws IOException{
+		
+		Pessoa paciente;
+		if(genero == "masc"){
+			paciente = new Homen()
+		}
+		bd.atualizaBD(users);
+	}
+	
+	public void buscaPaciente(Pessoa pessoa) throws IOException{
+		
+		bd.atualizaBD(users);
+	}
+	
+	public void updatePaciente(Pessoa pessoa) throws IOException{
+		
+		bd.atualizaBD(users);
+	}
+	
+	public void deletaPaciente(Pessoa pessoa) throws IOException{
+		
+		bd.atualizaBD(users);
+	}
+	
+	public void consultar(Pessoa pessoa){
+		
+	}
+	
+}
